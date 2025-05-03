7228f0a3f9ac2c2bde6942409a7239a301b73586
Alex-N-GU <aneyman@zagmail.gonzaga.edu>
2025-05-02 22:28:03 -0700

    Add files via upload
    testing file just the names for functions based off the specs we have made so far not sure how we want to proceed

---
+++ River_testing.cpp
@@ -0,0 +1,40 @@
+#include "BST.hpp"
+bool test_Info_Print(){
+
+    return false;
+}
+
+
+bool test_Nav_River(){
+
+
+    return false;
+}
+
+bool test_Update_Pos(){
+
+
+    return false;
+}
+
+bool test_Setup(){
+
+
+
+
+    return false;
+}
+int main(){
+
+    test_Info_Print();
+    test_Nav_River();
+    test_Update_Pos();
+    test_Setup();
+
+
+
+
+
+
+
+}
